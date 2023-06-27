271bdce6b9eccd0a389f4cf519bd38819955b5a4
Natalija_L <natalija.liske@gmail.com>
2023-06-27 14:34:16 +0300

    001

---
+++ 001/app.js
@@ -0,0 +1,35 @@
+console.log('Hello world');
+
+//let a = 10;
+//const b = 20;
+
+//console.log(typeof a, a);
+
+//a = a + 1;
+
+//console.log(typeof a, a);
+
+//let c = 1;
+
+//if(c-- && c) {
+//    console.log('JES');
+//}
+//else {
+ //   console.log('NO')
+//}
+
+let result;
+let digit = 5;
+
+if (3 > digit) {
+    result = 'YES'
+}
+else {
+    result = 'NO';
+}
+
+console.log(result);
+
+3 > digit && (result = 'YES');
+
+console.log(result);
